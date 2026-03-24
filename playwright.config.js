154deb200f0de53a0f284870d509a23611c1312e
Silviya Hristova <bliznetatata@gmail.com>
2026-03-24 18:52:02 +0000

    test: add playwright test for for devices
    add multiple device testing and screenshot after each test passes and failed

--- playwright.config.js
+++ playwright.config.js
@@ -8,4 +8,25 @@ module.exports=defineConfig({
         baseURL: 'http://127.0.0.1:5000', // base URL for the Flask app
     }, 
     outputDir: 'playwright-test-results/', // directory to store test results
+    screenshotDir: 'screenshots/', // directory to store screenshots
+    projects:[ //testing on different viewports to ensure responsive design
+        { 
+            name: `Desktop`,
+            use: { 
+                viewport: { width: 1280, height: 720 }, // set viewport size for desktop
+            },
+        },
+        {   
+            name: `Tablet`,
+            use: { 
+                viewport: { width: 768, height: 1024 }, // set viewport size for tablet
+            },
+        },
+        { 
+            name: `Mobile`,
+            use: { 
+                viewport: { width: 375, height: 667 }, // set viewport size for mobile
+            },
+        },
+    ],
 });
--- tests/frontend/base.spec.js
+++ tests/frontend/base.spec.js
@@ -1,5 +1,20 @@
 const {test, expect} = require('@playwright/test');
 
+// Take a screenshot after each test if fail and save it in the screenshots directory
+test.afterEach(async ({page}, testInfo) => {
+    const name = testInfo.title.replace(/\s+/g, '_'); // create a filename based on the test title
+    const device = testInfo.project.name; // get the device name from the project configuration
+
+    let folder = 'screenshots/failed'; // default folder for failed tests
+
+    if (testInfo.status !== testInfo.expectedStatus) { // only take a screenshot if the test failed
+        folder = 'screenshots/failed'; // folder for failed tests
+    } else 
+    if (testInfo.status === 'passed') {
+        folder = 'screenshots/passed'; // folder for passed tests
+    }
+    await page.screenshot({path: `${folder}/${name}_${device}.png`, fullPage: true}); // take a screenshot after each test
+});
 test.describe('Base layout', () => {
     test('homepage loads successfully and navbar and footer are visible', async ({page}) => {
         await page.goto('/'); // navigate to the homepage
