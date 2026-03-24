b691238e1ac812e53dddaa90246ec9d0f5abcfd2
Silviya Hristova <bliznetatata@gmail.com>
2026-03-24 19:09:40 +0000

    test: logo testing

--- tests/frontend/base.spec.js
+++ tests/frontend/base.spec.js
@@ -1,6 +1,7 @@
 const {test, expect} = require('@playwright/test');
 
 // Take a screenshot after each test if fail and save it in the screenshots directory
+const fs = require('fs');
 test.afterEach(async ({page}, testInfo) => {
     const name = testInfo.title.replace(/\s+/g, '_'); // create a filename based on the test title
     const device = testInfo.project.name; // get the device name from the project configuration
@@ -20,5 +21,14 @@ test.describe('Base layout', () => {
         await page.goto('/'); // navigate to the homepage
         await expect(page.locator('nav')).toBeVisible(); // check if navbar is visible
         await expect(page.locator('footer')).toBeVisible(); // check if footer is visible
+    });
+
+    test(`logo test`, async ({page}) => {
+        await page.goto('/'); // navigate to the homepage
+        const logo = page.locator('.nav-logo-link'); // locate the logo in the navbar
+        await expect(logo).toBeVisible(); // check if the logo is visible
+        await expect(logo).toHaveAttribute('href', '/'); // check if the logo links to the homepage
+        await logo.click(); // click the logo
+        await expect(page).toHaveURL(/\/$/); // check if the URL is still the homepage after clicking the logo
     });
 });
