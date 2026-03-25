bbb222a17f28fb2285ff78ec79abf19411cd9be3
Silviya Hristova <bliznetatata@gmail.com>
2026-03-25 14:07:16 +0000

    feat:add routes in routes.py for the html pages
    update url links in base.html in nav and footer

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,23 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('index.html')
+
+@main.route('/discover')
+def discover():
+    return render_template('discover.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+@main.route('/register')
+def register():
+    return render_template('register.html')
+
+@main.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@main.route('/support')
+def support():
+    return render_template('support.html')
