d6149d9138aec8942844e80301cc6dd3d7679f7d
Silviya Hristova <bliznetatata@gmail.com>
2026-04-04 04:13:45 +0100

    test: add register page dubplicate user and email test

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,3 +1,7 @@
+from werkzeug.security import generate_password_hash
+from app.models import User
+from app import db
+
 #test if the register page loads correctly
 def test_register_page_loads(client):
     response = client.get('/register')
@@ -40,3 +44,20 @@ def test_register_invalid_email(client):
 
     assert response.status_code == 200
     assert b'Please enter a valid email address.' in response.data
+
+#test for duplicate username or email
+def test_register_duplicate_username_email(client, app):
+    with app.app_context():
+        user = User(username='testuser', email='testuser@example.com', password=generate_password_hash('password123'))
+        db.session.add(user)
+        db.session.commit()
+
+    response = client.post('/register', data={
+        'username': 'testuser',
+        'email': 'testuser@example.com',
+        'password': 'password123',
+        'confirm_password': 'password123'
+    }, follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b'Username or email already exists.' in response.data
