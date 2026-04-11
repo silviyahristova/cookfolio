ab2021244dbe03748a1cd2b2043195329d46d737
Silviya Hristova <bliznetatata@gmail.com>
2026-04-11 19:51:05 +0100

    chore: configure flask-login LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from flask_login import LoginManager
 
 load_dotenv()
+
+login_manager = LoginManager()
 
 class Base(DeclarativeBase):
     pass
@@ -20,6 +23,12 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
 
     db.init_app(app)
+
+    login_manager.init_app(app)
+    # Configure Flask-Login
+    login_manager.login_view = 'main.login'
+    login_manager.login_message_category = 'warning'
+    login_manager.login_message = 'Please log in to access this page.'
 
     from .routes import main
     app.register_blueprint(main)
