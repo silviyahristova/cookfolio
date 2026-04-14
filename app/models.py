d7b9935bf6638166c7cadc5d8d8ee69d9a8ffa7e
Silviya Hristova <bliznetatata@gmail.com>
2026-04-14 13:49:36 +0100

    feat: add recipe model and migration
    created recipe model with user relationship and upgrade flask-migrate database

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Boolean, DateTime, func
+from sqlalchemy import String, Boolean, DateTime, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column
 from flask_login import UserMixin
 from . import db, login_manager
@@ -18,3 +18,17 @@ class User(db.Model, UserMixin):
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(User, int(user_id))
+
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    ingredients: Mapped[str] = mapped_column(String(1000), nullable=False)
+    instructions: Mapped[str] = mapped_column(String(2000), nullable=False)
+    prep_time: Mapped[int] = mapped_column(nullable=False)
+    cook_time: Mapped[int] = mapped_column(nullable=False)
+    servings: Mapped[int] = mapped_column(nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(),nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
