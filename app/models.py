e7bd2e03d048b677f4b79c129571ff5bd94eefab
Silviya Hristova <bliznetatata@gmail.com>
2026-04-27 10:27:31 +0100

    feat: add category model and relationships
    add relationships between the models

--- app/models.py
+++ app/models.py
@@ -14,17 +14,31 @@ class User(db.Model, UserMixin):
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(),nullable=False)
 
+    #Relationship to recipe- one user can have many recipes, and each recipe belongs to one user
+    recipes = db.relationship('Recipe', back_populates='user')
+
 # Flask-Login user loader
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(User, int(user_id))
 
+#Category model
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(db.String(50), unique=True, nullable=False)
+
+    #Relationship to recipe- one category can have many recipes, and each recipe belongs to one category
+    recipes = db.relationship('Recipe', back_populates='category')
+
+# Recipe model
 class Recipe(db.Model):
     __tablename__ = 'recipes'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100), nullable=False)
-    category: Mapped[str] = mapped_column(String(50), nullable=False)
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'), nullable=False)
     ingredients: Mapped[str] = mapped_column(String(1000), nullable=False)
     instructions: Mapped[str] = mapped_column(String(2000), nullable=False)
     prep_time: Mapped[int] = mapped_column(nullable=False)
@@ -32,3 +46,9 @@ class Recipe(db.Model):
     image_filename: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(),nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+
+    #Relationship to category- many recipes can belong to one category, and each recipe belongs to one category
+    category = db.relationship('Category', back_populates='recipes')
+
+    #Relationship to user- many recipes can belong to one user, and each recipe belongs to one user
+    user = db.relationship('User', back_populates='recipes')
