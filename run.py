a5bb450e5f28ba732bb517381b75869a4d0292c8
Silviya Hristova <bliznetatata@gmail.com>
2026-05-26 16:23:22 +0100

    chore: disable debug mode for production

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from app import create_app
 
 app = create_app()
 
+# Run the Flask application for production use,
+# with debug mode disabled
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
